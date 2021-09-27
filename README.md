36cc4604f8fbc94c7d192a9f66336127deef04c6
Felipe Tadeu <souzalima@gmail.com>
2021-09-27 00:04:39 -0300

    first eval

--- README.md
+++
@@ -1,2 +0,0 @@
-# 42-pipex
-pipex project for 42 Sao Paulo
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: ftadeu-d <ftadeu-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 20:54:03 by ftadeu-d          #+#    #+#             */
-/*   Updated: 2021/09/26 22:52:36 by ftadeu-d         ###   ########.fr       */
+/*   Updated: 2021/09/27 00:03:47 by ftadeu-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,10 @@ char	*valid_cmd(char *cmd, char **envp);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	print_error(void);
 void	run_cmd(char *argv, char **envp);
 void	left_process(char **argv, char **envp, int *fd);
 void	right_process(char **argv, char **envp, int *fd);
-
 
 #endif
